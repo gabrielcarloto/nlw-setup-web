fcba42e8e1ac6cb1e2780a79bc99cbe0c2f8972f
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-17 16:31:44 -0300

    refactor: remove unused code and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-// import './App.css';
 import './styles/global.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <div className="w-4 h-4 bg-zinc-700"></div>;
 }
 
