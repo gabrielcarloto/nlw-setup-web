a94d40ace64344c43ec11f03d8223d68236335ed
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-19 15:02:05 -0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Header from './components/Header';
 import './styles/global.css';
 
 function App() {
-  return <div className="w-4 h-4 bg-background"></div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
