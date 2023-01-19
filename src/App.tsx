17f083da4c004b876d4b7cdd4bb212bedd8069a3
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-19 14:44:59 -0300

    feat: add background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/global.css';
 
 function App() {
-  return <div className="w-4 h-4 bg-zinc-700"></div>;
+  return <div className="w-4 h-4 bg-background"></div>;
 }
 
 export default App;
