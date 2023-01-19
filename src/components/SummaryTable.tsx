fd108d356c9a9739008a26f5c94c977fb595482b
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-01-19 15:26:23 -0300

    feat: add habits table from dates

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 import HabitDay from './HabitDay';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minSummaryDatesSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minSummaryDatesSize - summaryDates.length;
 
 export default function SummaryTable() {
   return (
@@ -17,9 +23,17 @@ export default function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {Array.from(Array(120)).map(() => (
-          <HabitDay />
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
         ))}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+            <div
+              key={i}
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </div>
     </div>
   );
