531185601bb522ab6f93ecec76c648954ebf0591
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-02-01 14:52:48 -0300

    feat: add habits info

---
+++ src/components/HabitsList.tsx
@@ -0,0 +1,55 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
+
+interface HabitsInfo {
+  possibleHabits: Array<{
+    id: string;
+    title: string;
+    created_at: string;
+  }>;
+  completedHabits?: string[];
+}
+
+interface HabitsListProps {
+  date: Date;
+}
+
+export default function HabitsList({ date }: HabitsListProps) {
+  const [habitsInfo, setHabitInfo] = useState<HabitsInfo>();
+
+  useEffect(() => {
+    api
+      .get('day', {
+        params: {
+          date: date.toISOString(),
+        },
+      })
+      .then((res) => setHabitInfo(res.data));
+  }, []);
+
+  return (
+    <>
+      <div className="mt-6 flex flex-col gap-3">
+        {habitsInfo?.possibleHabits.map((habit) => (
+          <Checkbox.Root
+            key={habit.id}
+            checked={habitsInfo.completedHabits?.includes(habit.id)}
+            className="items-center flex gap-3 group"
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="font-semibold text-xl text-white leading-tight  group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        ))}
+      </div>
+    </>
+  );
+}
